561ae84521f23271b320ef87b154c34a0d32ba05
michaelmocioiu <michaelmocioiu@gmail.com>
2023-05-15 08:54:46 -0400

    all

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,22 +20,70 @@ class Board {
             ["", "", "", "", "", "b_S", "", "", "", "", "",],
             ["", "", "", "b_S", "b_S", "b_S", "b_S", "b_S", "", "", "",],
         ];
+        this.squares = this.getSquares()
     }   
-    setupBoard(){
+    getSquares(){
+        const squares = []
         for (let x = 0; x < this.row_s.length; x++){
             for (let y = 0; y < this.columns.length; y++){
-    
-                // console.log(x,y)
-                // console.log(this.config[x][y])
-                if (!!this.config[y][x]){
-                    document.getElementById(`${this.row_s.charAt(x)}${this.columns[y]}`).innerHTML = (`<button class="piece"><img src="assets/img/${this.config[y][x]}.png"></button>`)
-                }
+                squares.push(`${this.row_s[x]}${this.columns[y]}`)
             }
         }
-        document.getElementById('game_start').style.visibility = 'hidden'
-        document.getElementById('in_progress').style.visibility = 'visible'
+        console.log(squares)
+        return squares
+    }
+
+    buttonClick(option){
+        document.getElementById(option).style.boxShadow = '4px 4px 9px 0px rgba(0,0,0,0.52), inset 0px 0px 27px 0px rgba(0,0,0,0.66)'
+        setTimeout(function(){document.getElementById(option).style.boxShadow = '4px 4px 9px 0px rgba(0,0,0,0.52)'}, 200)
+
+        if (option === 'game_start'){
+            this.setupBoard()
+            document.getElementById('game_start').style.visibility = 'hidden'
+            document.getElementById('in_progress').style.visibility = 'visible'
+
+        }else if (option === 'reset_board'){
+            if (confirm('Are you sure?')){
+                for (let x = 0; x < this.row_s.length; x++){
+                    for (let y = 0; y < this.columns.length; y++){
+                        document.getElementById(`${this.row_s.charAt(x)}${this.columns[y]}`).innerHTML = (``)
+                        if (!!this.config[y][x]){
+                            document.getElementById(`${this.row_s.charAt(x)}${this.columns[y]}`).innerHTML = (`<button onclick="" class="piece ${this.config[y][x]}"><img src="assets/img/${this.config[y][x]}.png"></button>`)
+                        }
+                    }
+                }
+            }
+
+        }else if (option === 'undo'){
+            console.log('do')
+
+        }else if (option === 'redo'){
+            console.log('do')
+        }
+    }
+
+    buttonVisual(){
+
+    }
+
+    setupBoard(){
+        let c = 0
+        for (let x = 0; x < this.row_s.length; x++){
+            for (let y = 0; y < this.columns.length; y++){
+                if (!!this.config[y][x]){
+                    document.getElementById(this.squares[c]).innerHTML = (`<button onclick="" class="piece ${this.config[y][x]}"><img src="assets/img/${this.config[y][x]}.png"></button>`)
+                }
+                c++
+            }
+        }
+    }
+    clickHandle(){
+        x = 2
     }
 }
 
 mainBoard = new Board()
 
+class Square {
+
+}
